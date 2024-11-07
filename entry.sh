e66e4cfd5f54ab20e0bcadec1c38dd6b039d5498
woshikedayaa <woshikedayaa@gmail.com>
2024-11-07 16:39:50 +0800

    fix: can not run success

--- entry.sh
+++ entry.sh
@@ -5,13 +5,11 @@ PATH=/bin:/sbin
 echo "NAIVEPROXY_VERSION: ${NAIVEPROXY_VERSION}"
 if [ $# -gt 0 ];then
     exec /usr/bin/naive $@;
-    exit 0;
 fi
 
 if [ ! -f /etc/naive/config.json ];then
     rm -rf /etc/naive/config.json;
     echo '{"listen":"${SCHEME}://${LISTEN}:${PORT}","proxy":"${PROXY}"}' > /etc/naive/config.json;
-    exit 0;
 fi
 
 # run from env;
